976d575ebdcf4dc70cd2bd7ce0310a477116537c
abrar-rahman <abrar2016132@gmail.com>
2024-02-18 13:50:48 +0600

    buttonAndTableAdded

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+const cards = document.querySelectorAll(".card");
+// console.log(cards);
+
+
+for (let index = 0; index < cards.length; index++) {
+  const card = cards[index];
+  // console.log(card);
+
+  card.addEventListener('click', function () {
+    console.log("clicked");
+  })
+  
+}
