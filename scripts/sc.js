b6db41309e1802b75f6485789c600d4d02e29f4c
abrar-rahman <abrar2016132@gmail.com>
2024-02-18 20:53:07 +0600

    lotRemaining

---
+++ scripts/sc.js
@@ -0,0 +1,4 @@
+function setBackgroundColorByClass(elementId) {
+  const element = document.getElementsByClassName(elementId);
+  element.classList.add('bg-[#1DD100]');
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,32 @@
-let titleCount = 1;
 
 
-const cards = document.querySelectorAll(".nabisco");
-// console.log(cards);
+
+const cards = document.querySelectorAll(".card");
+
 
 
 for (let index = 0; index < cards.length; index++) {
   const card = cards[index];
-  // console.log(card);
+  // console.log(card.innerText);
 
   card.addEventListener('click', function () {
-    // console.log("clicked");
+    console.log("clicked");
 
-    const seatNumber = card.querySelector("h3").innerText;
-    console.log(seatNumber);
-
-
-    const seatTable = document.getElementById("seatCount");
-
-    const tr = document.createElement("tr");
-    tr.innerText = seatNumber + "Economy " + " 550 " ;
-
-    seatTable.appendChild(tr);
     
 
+
+
+
+   
+
+   
     
     
 
+    
+  
 
 
-
-
-  });
+  })
   
 }
